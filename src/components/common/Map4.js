747a8bda26aaa55bd9d00f0b7863c90399aa679f
tzerb <tzerb@hotmail.com>
2016-08-01 18:00:59 -0500

    Another try at mapping stuff.  It's a bit messed up.

--- src/components/common/Map4.js
+++ src/components/common/Map4.js
@@ -1,81 +1,132 @@
 import React, {PropTypes} from 'react';
 import toastr from 'toastr';
 import WaypointListRow from '../waypoints/WaypointListRow';
-
+// import GoogleMapsLoader from 'google-maps';
 
 const mapStyles = {
   content : {
-    height                : '500px',
+    height                : '500px'
   }
-}
+};
 
 class Map4 extends React.Component {
   constructor(props, context) {
     super(props, context);
 
+    let center = null;
+
+    try {
+
+      // GoogleMapsLoader.load(function(google) {
+      //   center = new google.maps.LatLng(44.012077, -89.40526);
+      // });
+      
+      this.hasMap = true;
+    }
+    catch(ex)
+    {
+      this.hasMap=false;
+    }
+
+    this.Markers = [];  
     this.state = {
       waypoints: Object.assign({}, this.props.waypoints),
       zoom: 6,
-      center: new google.maps.LatLng(44.012077, -89.40526)
+      center: this.center,
+      hasMap : this.hasMap
     };
 
     this.renderMap = this.renderMap.bind(this);
     this.buttonClicked = this.buttonClicked.bind(this);
-    
   }
 
   componentDidMount(prevProps,  prevState) {
     toastr.success('componentDidMount - ' + this._mapElement);
     toastr.success('componentDidMount - zoom = ' + this.state.zoom);
 
-    try {
-      let _this = this;
-      let mapElement = document.getElementById("map_canvas");
-      //let mapElement = this._mapElement;
+    // GoogleMapsLoader.KEY = 'AIzaSyCG_KuXMd6mrgAzrRcgXr91Yr6Ed03VNaw';
+    // GoogleMapsLoader.LIBRARIES = ['geometry', 'places', 'visualization'];    //geometry,places,visualization
+    // GoogleMapsLoader.load(function(google) {
 
-      let myLatlng = new google.maps.LatLng(44.012077, -89.40526);
-      let options = {
-          zoom: this.state.zoom,
-          center: this.state.center
-      }   
-      var map = new google.maps.Map(mapElement, options);
+      try {
 
-      // var infowindow = new google.maps.InfoWindow({
-      //   content: 'Change the zoom level',
-      //   position: myLatlng
-      // });
-      // infowindow.open(map);
+        if (this.hasMap)
+        {
+          let _this = this;
+          let mapElement = document.getElementById("map_canvas");
 
-      map.addListener('zoom_changed', function() {
-        _this.setState({zoom: map.getZoom()});
-      });
+          //let mapElement = this._mapElement;
 
-      map.addListener('dragend', function() {
-        _this.setState({center: map.getCenter()});
-      });
+          let myLatlng = new google.maps.LatLng(44.012077, -89.40526);
+          let options = {
+              zoom: this.state.zoom,
+              center: this.state.myLatlng
+          };
+          this._map = new google.maps.Map(mapElement, options);
 
-      if (this.props.waypoints)
+          // var infowindow = new google.maps.InfoWindow({
+          //   content: 'Change the zoom level',
+          //   position: myLatlng
+          // });
+          // infowindow.open(map);
+
+          this._map.addListener('zoom_changed', function() {
+            _this.setState({zoom: _this._map.getZoom()});
+          });
+
+          this._map.addListener('dragend', function() {
+            _this.setState({center: _this._map.getCenter()});
+          });
+    
+          this.hasMap = true;
+          this.addMarkers();
+        }
+        //this._mapElement.appendChild(mapElement);
+      } catch(ex)
+      {
+        this.hasMap = false;
+        toastr.error('componentDidMount - ' + ex);
+      }
+    // });
+    
+  }
+
+  componentDidUpdate (prevProps, prevState)
+  {
+    toastr.success('componentDidUpdate');
+    this.deleteMarkers();
+    this.addMarkers();
+  }
+
+  addMarkers()
+  {
+      if (this.hasMap && this.props.waypoints)
       {
         this.props.waypoints.map((wp) => 
         {
-          toastr.success(wp.name)
+          toastr.success(wp.name);
           let marker = new google.maps.Marker({
               position: new google.maps.LatLng(wp.latitude, wp.longitude),
-              map: map,
+              map: this._map,
               title: wp.name
           });
-          
+          this.Markers.push(marker);
         });
-      }   
-    } catch(ex)
+      }  
+  }
+
+  deleteMarkers()
+  {
+    for(let i = 0; i<this.Markers.length; i++)
     {
-      toastr.error('componentDidMount - ' + ex);
+      this.Markers[i].setMap(null);
     }
   }
 
   buttonClicked() {
     this.setState({zoom: this.state.zoom+1});
   }
+
   renderMap(el)
   {
   }
@@ -112,7 +163,7 @@ class Map4 extends React.Component {
           )} 
         </tbody>
       </table>
-      <div width="200">{this.state.zoom}<br/>{this.state.center.lat()}, {this.state.center.lng()}</div><button onClick={this.buttonClicked}>zoom</button>
+      <div width="200">{this.state.zoom}<br/>{'hasMap = ' + this.state.hasMap}</div><button onClick={this.buttonClicked}>zoom</button>
       <div id="map_canvas4" style={mapStyles} ref={(c) => this._mapElement = c}></div>
       </div>
     }
